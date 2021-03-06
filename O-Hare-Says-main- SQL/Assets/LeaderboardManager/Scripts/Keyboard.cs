d69ea5f5f43635f4e966ed40f1f0c0ff2ba4b7f7
joeh28 <76986336+joeh28@users.noreply.github.com>
2021-03-06 10:40:36 -0500

    Add Keyboard; update logo

--- O-Hare-Says-main- SQL/Assets/LeaderboardManager/Scripts/Keyboard.cs
+++ O-Hare-Says-main- SQL/Assets/LeaderboardManager/Scripts/Keyboard.cs
@@ -2,22 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Keyboard : MonoBehaviour
 {
     string word = null;
     int wordIndex = 0;
     string alpha;
-    public Text TextName = null;
-    
+    public TMP_Text TextLetters = null;
+    public Text TextLetters2 = null;
+
+
     // Use this for initialization
 
     public void alphabetFunction(string alphabet)
     {
         wordIndex++;
         word = word + alphabet;
-        TextName.text = word;
-        
+        TextLetters.text = word;
+        TextLetters2.text = word;
+
 
     }
 }
